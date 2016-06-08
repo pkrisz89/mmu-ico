cb83682b729e7257b5168008a9a33550f9d754cc
Kris Pinter <pinter.krisztian89@gmail.com>
2016-06-08 20:32:26 +0100

    boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var minifyCSS = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var runSequence = require('run-sequence');
+var prettify = require('gulp-html-prettify');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+// Basic Gulp task syntax
+gulp.task('hello', function() {
+  console.log('Hello Zell!');
+})
+
+// Development Tasks
+// -----------------
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/sass/**/*.scss') // Gets all files ending with .scss in app/sass and children dirs
+    .pipe(sass()) // Passes it through a gulp-sass
+    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+// Watchers <--- These are constantly looking for changes in scss, html and js files
+gulp.task('watch', function() {
+  gulp.watch('app/sass/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+// Optimization Tasks
+// ------------------
+
+
+
+// Minify+Autoprefix+Concat CSS
+
+gulp.task('css', function(){
+    gulp.src('app/css/*.css')
+    .pipe(minifyCSS())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('dist/css'))
+});
+
+// Minifying JavaScript files
+gulp.task('js', function(){
+    gulp.src('app/js/*.js')
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('dist/js'))
+});
+
+
+// Optimizing Images
+gulp.task('images', function() {
+  return gulp.src('app/img/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true,
+    })))
+  .pipe(gulp.dest('dist/img'))
+});
+
+// Copying fonts
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+// Prettifying HTML
+gulp.task('templates', function() {
+  gulp.src('app/*.html')
+    .pipe(prettify({indent_char: ' ', indent_size: 2}))
+    .pipe(gulp.dest('dist/'))
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync', 'watch'],
+    callback
+  )
+})
+
+gulp.task('build', function(callback) {
+  runSequence(
+    ['css', 'js' , 'images', 'fonts', 'templates'],
+    callback
+  )
+})
