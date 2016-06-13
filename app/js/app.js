9fc0ef952dcfe7ea940e3ede4f8ae02d3d9e9b4f
Kris Pinter <Kris.Pinter@hq.peninsula-uk.local>
2016-06-13 17:19:36 +0100

    changed navigation to an object

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,11 @@
 (function(){
+    
 
 var app = angular.module("studentApp", ['ngRoute']); 
 
 app.controller('dataController', function($http) {
     var vm = this;
+    
     $http.get("students.json").then(function(response) {
         vm.yearOne = response.data.yearOne;
         vm.yearTwo = response.data.yearTwo;
@@ -11,10 +13,46 @@ app.controller('dataController', function($http) {
         vm.yearThree = response.data.yearThree;
         vm.graduates = response.data.graduates;
     });
+    
+    vm.pages = [
+            {
+                'name': 'Home',
+                'active': false,
+                'url': '#/'
+            },
+            {
+                'name': 'Year One',
+                'active': false,
+                'url': '#/year-one'     
+            },
+            {
+                'name': 'Year Two',
+                'active': false,
+                'url': '#/year-two'
+            },
+            {
+                'name': 'Year Three',
+                'active': false,
+                'url': '#/year-three'
+            },
+            {
+                'name': 'Graduates',
+                'active': false,
+                'url': '#/graduates'
+            }];
+    
+    
+    vm.setActive = function(page) {
+        //for each vm.pages, set active false, then set current page active to true
+        // OR
+        // change this to set the current page to an object, check currentPage.name == page.name in view
+        //this.page = {};
+        page.active = true;
+    }
     });
 
     
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
     $routeProvider
     .when("/", {
         templateUrl : "templates/home.html"
@@ -35,7 +73,10 @@ app.config(function($routeProvider) {
         templateUrl : "templates/404.html"
     })
     .otherwise({ redirectTo: '/404' });
+    
+    //$locationProvider.html5Mode(true);
 });
+    
     
 }());
 
